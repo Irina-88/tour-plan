0fdc4fb440fc83a525408cf5f181df0364b7e1f1
Irina-88 <lazutkairinna@gmail.com>
2021-05-25 08:51:06 +0200

    Create: added feedback block

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-const hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-  effect: 'coverflow',
-   autoplay: {
-   delay: 3000,
- },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-  },
-});
-const reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
+$(document).ready(function () {
+  
 });
