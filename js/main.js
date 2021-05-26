4584cc52b0b73a71c33dd50be44f57459adbd520
Irina-88 <lazutkairinna@gmail.com>
2021-05-26 22:47:23 +0200

    Сonnected form validation and mask for numbers

--- js/main.js
+++ js/main.js
@@ -47,7 +47,30 @@ function closeModal(event) {
   event.preventDefault();
   var modalOverlay = $('.modal__overlay');
    var modalDialog = $('.modal__dialog');
-   modalOverlay.removeClass("modal__overlay--visible");
-   modalDialog.removeClass("modal__dialog--visible");
+   modalOverlay.removeClass('modal__overlay--visible');
+   modalDialog.removeClass('modal__dialog--visible');
 }
+// Обработка форм
+  $('.form').each(function (){
+    $(this).validate({
+      errorClass: 'invalid',
+      messages: {
+        name: {
+          required: 'Please specify your name',
+          minlength: 'name must be at least 2 letters',
+        },
+        email: {
+          required: 'We need your email address to contact you',
+          email: 'Your email address must be in the format of name@domain.com',
+        },
+        phone: {
+          required: 'Your phone number is required',
+        },
+      },
+    });
+  })
+  // Маска номера телефона
+  $(document).ready(function(){
+    $('[type="tel"]').mask('+7 (999) 999-99-99')
+  });
 });
